afd11aa85be0fdada3a8697e30d0f83307777842
development0264 <development0264@gmail.com>
2022-06-13 18:44:33 +0530

    Manage operating manual list : Done

--- src/pages/PrintPDF/PdfComponent.jsx
+++ src/pages/PrintPDF/PdfComponent.jsx
@@ -1,29 +1,25 @@
-import { faPrint } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { Button } from "react-bootstrap";
-import ReactToPrint from "react-to-print";
-import DataComponent from "./DataComponent";
+import { faPrint } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import ReactToPrint from 'react-to-print';
+import DataComponent from './DataComponent';
 
 class PdfComponent extends React.Component {
   render() {
     return (
       <div>
-        <ReactToPrint
-          content={() => this.componentRef}
-          trigger={() => (
-            <div className="print_button">
-              <Button>
-                {" "}
-                <FontAwesomeIcon icon={faPrint} />
-              </Button>
-            </div>
-          )}
-        />
         <DataComponent
           {...this.props}
           ref={(response) => (this.componentRef = response)}
         />
+        <div className='bottom_button'>
+          <Button variant="outline">View Here</Button>
+          <ReactToPrint
+            content={() => this.componentRef}
+            trigger={() => <Button>Save PDF</Button>}
+          />
+        </div>
       </div>
     );
   }
