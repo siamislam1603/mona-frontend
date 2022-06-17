dcf3b600693ce5a7cec194fd3e97fdbf7dab4814
Rudra Gaur <irudragaur@gmail.com>
2022-06-17 09:28:34 +0530

    data is being saved in franchisee form.

--- src/pages/NewFranchisees.jsx
+++ src/pages/NewFranchisees.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Row, Col, Button, Container } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import TopHeader from '../components/TopHeader';
 import LeftNavbar from '../components/LeftNavbar';
+import Select from 'react-select';
 const NewFranchisees = () => {
+
+    const [franchiseeData, setFranchiseeData] = useState();
+
+    const handleChange = event => {
+        const { name, value } = event.target;
+
+        setFranchiseeData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    } 
+
+    console.log('FRANCHISEE DATA:', franchiseeData);
+
     return (
         <div>
             <div id="main">
@@ -28,78 +43,135 @@ const NewFranchisees = () => {
                                         <Col sm={6} md={6} lg={6}>
                                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                                 <Form.Label>Franchise Name</Form.Label>
-                                                <Form.Control type="text" placeholder="Special DayCare" />
+                                                <Form.Control
+                                                    name="franchisee_name" 
+                                                    type="text" 
+                                                    placeholder="Special DayCare"
+                                                    onChange={handleChange} />
                                             </Form.Group>
 
                                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                                 <Form.Label>ABN</Form.Label>
-                                                <Form.Control type="text" placeholder="6743433" />
+                                                <Form.Control
+                                                    name="abn" 
+                                                    type="text" 
+                                                    placeholder="6743433"
+                                                    onChange={handleChange} />
                                             </Form.Group>
 
-                                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                            <Form.Group className="mb-3">
                                                 <Form.Label>Suburb</Form.Label>
-                                                <Form.Select aria-label="Default select example">
-                                                    <option>Sydney</option>
-                                                    <option value="1">Sydney</option>
-                                                    <option value="2">Sydney</option>
-                                                    <option value="3">Sydney</option>
-                                                </Form.Select>
+                                                <Select
+                                                placeholder="Which Suburb?"
+                                                closeMenuOnSelect={true}
+                                                // options={cityData}
+                                                onChange={(e) =>
+                                                    setFranchiseeData((prevState) => ({
+                                                        ...prevState,
+                                                        city: e.value,
+                                                    }))
+                                                }
+                                                />
                                             </Form.Group>
 
-                                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                            <Form.Group className="mb-3">
                                                 <Form.Label>State</Form.Label>
-                                                <Form.Select aria-label="Default select example">
-                                                    <option>Western Australia</option>
-                                                    <option value="1">Western Australia</option>
-                                                    <option value="2">Western Australia</option>
-                                                    <option value="3">Western Australia</option>
-                                                </Form.Select>
+                                                <Select
+                                                placeholder="Which State?"
+                                                closeMenuOnSelect={true}
+                                                // options={cityData}
+                                                onChange={(e) =>
+                                                    setFranchiseeData((prevState) => ({
+                                                        ...prevState,
+                                                        state: e.value,
+                                                    }))
+                                                }
+                                                />
                                             </Form.Group>
+
                                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                                 <Form.Label> Email Address  </Form.Label>
-                                                <Form.Control type="email" placeholder="admin@specialdaycare.com" />
+                                                <Form.Control
+                                                    name="email" 
+                                                    type="email" 
+                                                    placeholder="admin@specialdaycare.com"
+                                                    onChange={handleChange} />
                                             </Form.Group>
-                                            <Form.Group className="mb-3" controlId="formBasicPassword">
+
+                                            <Form.Group className="mb-3">
                                                 <Form.Label>Franchisee Admin</Form.Label>
-                                                <Form.Select aria-label="Default select example">
-                                                    <option>Andy Smith</option>
-                                                    <option value="1">Andy Smith</option>
-                                                    <option value="2">Andy Smith</option>
-                                                    <option value="3">Andy Smith</option>
-                                                </Form.Select>
+                                                <Select
+                                                placeholder="Select Franchisee Admin"
+                                                closeMenuOnSelect={true}
+                                                // options={cityData}
+                                                onChange={(e) =>
+                                                    setFranchiseeData((prevState) => ({
+                                                        ...prevState,
+                                                        franchisee_admin: e.value,
+                                                    }))
+                                                }
+                                                />
                                             </Form.Group>
                                         </Col>
+
                                         <Col sm={6} md={6} lg={6}>
                                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                                 <Form.Label>Franchise Number</Form.Label>
-                                                <Form.Control type="text" placeholder="ADS 00342" />
+                                                <Form.Control 
+                                                    name="franchisee_number"
+                                                    type="text" 
+                                                    placeholder="ADS 00342"
+                                                    onChange={handleChange} />
                                             </Form.Group>
 
                                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                                 <Form.Label>ACN</Form.Label>
-                                                <Form.Control type="text" placeholder="3453453453" />
+                                                <Form.Control
+                                                    name="acn" 
+                                                    type="text" 
+                                                    placeholder="3453453453"
+                                                    onChange={handleChange} />
                                             </Form.Group>
 
                                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                                 <Form.Label>Street Address</Form.Label>
-                                                <Form.Control type="text" placeholder="5th Avenue, Central Park Street, Broadway" />
+                                                <Form.Control 
+                                                    name="address"
+                                                    type="text" 
+                                                    placeholder="5th Avenue, Central Park Street, Broadway"
+                                                    onChange={handleChange} />
                                             </Form.Group>
+
                                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                                 <Form.Label>Postcode</Form.Label>
-                                                <Form.Control type="text" placeholder="24545" />
+                                                <Form.Control
+                                                    name="postcode" 
+                                                    type="text" 
+                                                    placeholder="24545"
+                                                    onChange={handleChange} />
                                             </Form.Group>
+
                                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                                 <Form.Label> Contact Number</Form.Label>
-                                                <Form.Control type="email" placeholder="admin@454 342 34.com" />
+                                                <Form.Control 
+                                                    name="contact"
+                                                    type="email" 
+                                                    placeholder="admin@454 342 34.com"
+                                                    onChange={handleChange} />
                                             </Form.Group>
+
                                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                                 <Form.Label>Franchisee Admin’s Email</Form.Label>
-                                                <Form.Control type="text" placeholder="andy.smith@specialdaycare.com" />
+                                                <Form.Control 
+                                                    name="franchisee_admin_email"
+                                                    type="text" 
+                                                    placeholder="andy.smith@specialdaycare.com" />
                                             </Form.Group>
                                         </Col>
+
                                         <div className="d-flex justify-content-center my-5">
                                             <Form.Group className="mb-3" controlId="formBasicPassword">
-                                                <Button variant="link">Link</Button>
+                                                <Button variant="link btn btn-light btn-md m-2" style={{ backgroundColor: '#efefef' }}>Cancel</Button>
                                                 <Button type="submit">Save Details</Button>
                                             </Form.Group>
                                         </div>
