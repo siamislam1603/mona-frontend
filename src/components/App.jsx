9ce809421f7b9efe05019789849419bfd38184ac
EC2 Default User <ec2-user@ip-172-31-26-97.ap-southeast-2.compute.internal>
2022-07-20 06:09:59 +0000

    resolved conflicts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import Main from "./Main";
 
-<<<<<<< HEAD
  const BASE_URL="http://3.26.39.12:4000";
 //const BASE_URL="http://127.0.0.1:4000";
-=======
-//  const BASE_URL="http://3.26.39.12:4000";
-const BASE_URL="http://127.0.0.1:4000";
->>>>>>> 88a1a2158bca7be3c39f731fbf638f9618ba26fb
 // const BASE_URL = "http://localhost:4000";
 
 function App() {
