6d290df1146d4a4614d302a4f48943d80852a797
unknown <rajatk@neuronimbus.com>
2022-07-20 17:58:36 +0530

    announcement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,8 @@ import React from 'react';
 import Main from "./Main";
 
 //  const BASE_URL="http://3.26.39.12:4000";
-<<<<<<< HEAD
-//const BASE_URL="http://127.0.0.1:4000";
-const BASE_URL = "http://localhost:4000";
-=======
 const BASE_URL="http://127.0.0.1:4000";
 // const BASE_URL = "http://localhost:4000";
->>>>>>> 76a5fdbe5b9d9f0f7fa3343a4d7306160829e90d
 
 function App() {
   return (
