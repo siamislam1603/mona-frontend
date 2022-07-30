1acf7ae0496e65d5f8ff1af4a81f15890a9bdca1
kartiks-diagright <kartik.sharma@diagright.com>
2022-07-30 12:43:05 +0530

    add and edit permissions added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -201,6 +201,7 @@ const Main = () => {
           element={
             <Protected isLoggedIn={isLoggedIn}>
               {/* <ForgotPassword /> */}
+              <SignIn />
               <AddPermissions />
             </Protected>
           }
