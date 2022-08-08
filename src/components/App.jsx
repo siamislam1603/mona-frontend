e80d846360b957e7a71ca6852cb000b16782dc91
EC2 Default User <ec2-user@ip-172-31-18-195.ap-southeast-2.compute.internal>
2022-08-08 14:30:45 +0000

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import Main from "./Main";
 
-<<<<<<< HEAD
 const BASE_URL = "http://3.26.39.12:4000";
 // const BASE_URL = "http://3.26.240.23:4000";
 // const BASE_URL = "http://127.0.0.1:4000";
 // const BASE_URL = "http://localhost:4000";
-=======
-// const BASE_URL = "http://3.26.39.12:4000";
-// const BASE_URL = "http://3.26.240.23:4000";
-// const BASE_URL = "http://127.0.0.1:4000";
-const BASE_URL = "http://localhost:4000";
->>>>>>> 9d550fa2ed5dc5702314d0f5f40a9d6cc48735f9
 
 function App() {
   return (
