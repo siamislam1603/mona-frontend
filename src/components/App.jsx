f6bbb7ae85bdc7a69633ba3f19c988fa148eb134
EC2 Default User <ec2-user@ip-172-31-26-97.ap-southeast-2.compute.internal>
2022-08-11 04:44:26 +0000

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,6 @@ import Main from "./Main";
 
 const BASE_URL = "http://3.26.39.12:4000";
 // const BASE_URL = "http://3.26.240.23:4000";
-<<<<<<< HEAD
-=======
-// const BASE_URL = "http://127.0.0.1:4000";
->>>>>>> 4dcf08c17a1ff790cb808ea2601745eb5310dfbe
 // const BASE_URL = "http://localhost:4000";
 
 function App() {
