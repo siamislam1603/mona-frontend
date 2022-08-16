d2b650cd3a9812834c0ea8bd369e9a45c98884fc
EC2 Default User <ec2-user@ip-172-31-26-97.ap-southeast-2.compute.internal>
2022-08-16 12:26:31 +0000

    changes

---
+++ src/pages/InputFields/Headings.jsx
@@ -0,0 +1,14 @@
+import { Form,Col } from "react-bootstrap";
+
+const Headings = (props) => {
+  const { ...controls } = props;
+  return (
+    <Col sm={12}>
+    <br/>
+    <Form.Group>
+      <Form.Label>{controls.field_label}</Form.Label>
+    </Form.Group>
+    </Col>
+  );
+};
+export default Headings;
