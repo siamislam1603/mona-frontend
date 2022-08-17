2bb7379624a49da8dfb427177e42b103ed507089
EC2 Default User <ec2-user@ip-172-31-18-195.ap-southeast-2.compute.internal>
2022-08-17 18:44:51 +0000

    changed by yatender in training detail and app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import Main from "./Main";
 
-<<<<<<< HEAD
 const BASE_URL = "http://3.26.39.12:4000";
 // const BASE_URL = "http://3.26.240.23:4000";
 // const BASE_URL = "http://127.0.0.1:4000";
 // const FRONT_BASE_URL = "http://localhost:5000";
 const FRONT_BASE_URL = "http://3.26.240.23:5000";
-=======
- const BASE_URL = "http://3.26.39.12:4000";
-// const BASE_URL = "http://127.0.0.1:4000";
-//const BASE_URL = "http://localhost:4000";
-
-const FRONT_BASE_URL="http://localhost:5000";
-// const FRONT_BASE_URL = "http://3.26.39.12:5000";
-// const FRONT_BASE_URL = "http://3.26.240.23:5000";
->>>>>>> 0a2629edb8227e43cdf87f818d81167db7c3a653
-
 
 function App() {
   return (
