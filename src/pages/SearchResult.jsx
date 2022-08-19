0fcc72fd9e956b03d1e2fcc7e6ebbabfd66abe5f
gurpreets@neuronimbus.com-at-116341680296 <grupreets@neuronimbus.com>
2022-08-19 15:09:15 +0530

    search changes

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import { BASE_URL } from '../components/App'
+import { Button, Container } from "react-bootstrap";
+import LeftNavbar from "../components/LeftNavbar";
+import TopHeader from "../components/TopHeader";
 
 const SearchResult = (props) => {
   const queryParams = new URLSearchParams(window.location.search)
@@ -21,7 +24,103 @@ const SearchResult = (props) => {
   },[term])
 
   return (
-    <div>SearchResult</div>
+    <div className="announcement-accordion">
+
+<Container>
+            <div className="admin-wrapper">
+              <aside className="app-sidebar">
+              <LeftNavbar />
+              </aside>
+              <div className="sec-column">
+                <TopHeader 
+                  notificationType='Child Enrollment'/>
+
+              <div className="entry-container searchresult-sec">
+                  <header className="title-head">
+                    <h1 className="title-lg">Search Result</h1>
+                  </header>
+                  
+                  <div className="column-card">
+                    <header className="entry-title mb-4">
+                      <h2 className="title-xs">Traning</h2>
+                    </header>
+                    <div className="search-item">
+                      <div className="search-user-pic">
+                        <a href="/">
+                          <figure className="figure"><img alt="" src="/img/related-pic1.png" className="figure-img img-fluid" /></figure>
+                        </a>
+                      </div>
+                      <div className="search-user-detail">
+                        <h2 className="title-md text-capitalize"><a href="/">Manita Vidyarthy</a></h2>
+                        <div className="totalview mb-2">
+                          <span className="style-scope meta-block">
+                            <strong>Shared Time:</strong> <time>38 minutes ago</time>
+                          </span>
+                        </div>
+                        <div className="user-link mt-4"><a href="/">View Details</a></div>
+                      </div>
+                    </div>
+                    <div className="search-item">
+                      <div className="search-user-pic">
+                        <a href="/">
+                          <figure className="figure"><img alt="" src="/img/related-pic2.png" className="figure-img img-fluid" /></figure>
+                        </a>
+                      </div>
+                      <div className="search-user-detail">
+                        <h2 className="title-md text-capitalize"><a href="/">Manita Vidyarthy</a></h2>
+                        <div className="totalview mb-2">
+                          <span className="style-scope meta-block">
+                            <strong>Shared Time:</strong> <time>38 minutes ago</time>
+                          </span>
+                        </div>
+                        <div className="user-link mt-4"><a href="/">View Details</a></div>
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <div className="column-card">
+                    <header className="entry-title mb-4">
+                      <h2 className="title-xs">Traning</h2>
+                    </header>
+                    <div className="search-item">
+                      <div className="search-user-pic">
+                        <a href="/">
+                          <figure className="figure"><img alt="" src="/img/related-pic3.png" className="figure-img img-fluid" /></figure>
+                        </a>
+                      </div>
+                      <div className="search-user-detail">
+                        <h2 className="title-md text-capitalize"><a href="/">Manita Vidyarthy</a></h2>
+                        <div className="totalview mb-2">
+                          <span className="style-scope meta-block">
+                            <strong>Shared Time:</strong> <time>38 minutes ago</time>
+                          </span>
+                        </div>
+                        <div className="user-link mt-4"><a href="/">View Details</a></div>
+                      </div>
+                    </div>
+                    <div className="search-item">
+                      <div className="search-user-pic">
+                        <a href="/">
+                          <figure className="figure"><img alt="" src="/img/related-pic4.png" className="figure-img img-fluid" /></figure>
+                        </a>
+                      </div>
+                      <div className="search-user-detail">
+                        <h2 className="title-md text-capitalize"><a href="/">Manita Vidyarthy</a></h2>
+                        <div className="totalview mb-2">
+                          <span className="style-scope meta-block">
+                            <strong>Shared Time:</strong> <time>38 minutes ago</time>
+                          </span>
+                        </div>
+                        <div className="user-link mt-4"><a href="/">View Details</a></div>
+                      </div>
+                    </div>
+                  </div>
+
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
