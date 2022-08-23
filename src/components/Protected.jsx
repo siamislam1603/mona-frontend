74b205d417cf085bf2f212ef2975074321b1428c
Rudra Gaur <irudragaur@gmail.com>
2022-08-23 09:50:54 +0530

    changes

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,5 +1,5 @@
 const Protected = ({ isLoggedIn, children }) =>  {
-  return typeof isLoggedIn === "undefined" || isLoggedIn === false ? children[0] : children[1];
+  return localStorage.getItem('is_user_logged_in') === 'logged_out' || typeof isLoggedIn === 'undefined' || isLoggedIn === false ? children[0] : children[1];
 }
 
 export default Protected;
