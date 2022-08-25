91bf26df89d05738005e04c8a2dcbb60e8619420
pankajg@neuronimbus.com-at-116341680296 <pankajg@neuronimbus.com>
2022-08-26 03:48:22 +0800

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import Main from "./Main";
 
-<<<<<<< HEAD
-
  const BASE_URL = "http://13.237.14.155:4000";
 // const BASE_URL = "http://127.0.0.1:4000";
 // const FRONT_BASE_URL = "http://3.26.39.12:5000";
-=======
-//  const BASE_URL = "http://13.237.14.155:4000";
-const BASE_URL = "http://127.0.0.1:4000";
->>>>>>> 1d9bdacea67cfc649ddb5184e8060031b134c534
+ const FRONT_BASE_URL = "http://13.237.14.155:5000";
 // const BASE_URL = "http://127.0.0.1:4000";
-const FRONT_BASE_URL = "http://localhost:5000";
 
 function App() {
   return (
