0da83ce78658b22201134e9444835331438aead6
Rudra Gaur <irudragaur@gmail.com>
2022-08-30 15:27:18 +0530

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Main from "./Main";
 
-const BASE_URL = "http://54.206.190.120:4000";
+// const BASE_URL = "http://54.206.190.120:4000";
 // const BASE_URL = "http://localhost:4000"
 // const BASE_URL = "http://127.0.0.1:4000";
-// const FRONT_BASE_URL = "http://3.26.39.12:5000";
-// const FRONT_BASE_URL = "http://13.237.14.155:5000";
+// const FRONT_BASE_URL = "http://3.26.39.12:4000";
+const BASE_URL = "http://13.237.14.155:4000";
 const FRONT_BASE_URL = "http://127.0.0.1:5000";
 
 function App() {
