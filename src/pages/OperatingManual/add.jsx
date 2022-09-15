8f6b131940b693c28dadffe4c5e70ebc5704f9cb
development0264 <development0264@gmail.com>
2022-09-15 11:36:45 +0530

    Fix the issue related to OM notification.

--- src/pages/OperatingManual/add.jsx
+++ src/pages/OperatingManual/add.jsx
@@ -183,6 +183,10 @@ const AddOperatingManual = () => {
         data['accessible_to_role'] = null;
         data['accessible_to_all'] = true;
       } else {
+        if(localStorage.getItem("user_role")!=="franchisor_admin")
+        {
+          formSettingData.shared_role =  formSettingData.shared_role ? formSettingData.shared_role.replace("franchisee_admin,","") : null;
+        }
         formSettingData.shared_role =  formSettingData.shared_role ? formSettingData.shared_role.replace("all,","") : null;
         data['shared_role'] = formSettingData.shared_role
           ? formSettingData.shared_role.slice(0, -1)
