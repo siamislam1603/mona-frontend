b224a0f4c0125f1d77720fd74f732a74b697e5ee
pardeepverma007 <pardeepv@neuronimbus.com>
2022-09-25 22:51:53 +0530

    File Resp

--- src/pages/FileRepository.jsx
+++ src/pages/FileRepository.jsx
@@ -228,7 +228,7 @@ const FileRepository = () => {
                                       value={SearchValue}
                                       onChange={HandelSearch} />
                                   </label>
-                                  {/* <SearchBar {...props.searchProps} /> */}
+                                
                                 </div>
                                 <FilerepoUploadFile  />
                               </div>
