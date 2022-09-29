32eb3058e2b6fdffce947074accf250042ce33e2
development0264 <development0264@gmail.com>
2022-09-29 10:10:12 +0530

    Manage text heading in form.

--- src/pages/InputFields/TextHeadings.jsx
+++ src/pages/InputFields/TextHeadings.jsx
@@ -3,7 +3,7 @@ import { Form,Col } from "react-bootstrap";
 const TextHeadings = (props) => {
   const { ...controls } = props;
   return (
-    <Col sm={12} className="main-form-heading-title">
+    <Col sm={12} className="main-form-text-heading-title">
     <br/>
     <Form.Group>
       <Form.Label className="form-style-headings">{controls.field_label}</Form.Label>
