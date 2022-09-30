7f8859bdcd437e68fe2502054ef69bc8d0a2f33b
Sobhan <sobhank@neuronimbus.com>
2022-09-30 16:47:40 +0530

    changes

--- src/pages/InputFields/TextArea.jsx
+++ src/pages/InputFields/TextArea.jsx
@@ -11,7 +11,7 @@ const TextArea = (props) => {
           as="textarea"
           rows={controls.row ? controls.row : 3}
           name={controls.field_name}
-          maxLength={1200}
+          maxLength={2000}
 
           className="child_input"
           placeholder={controls.placeholder}
@@ -22,7 +22,7 @@ const TextArea = (props) => {
           }}
           isInvalid={!!controls.error[controls.field_name]}
         />
-        <p style={{fontSize:"12px",marginBottom:"3px",marginLeft:"77%"}}>(Word Limit : 2000)</p>
+        <p style={{fontSize:"12px",marginBottom:"3px",marginLeft:"77%"}}>(Text Limit : 2000)</p>
         <Form.Control.Feedback type="invalid">
           {controls.error[controls.field_name]}
         </Form.Control.Feedback>
