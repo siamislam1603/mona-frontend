4675f1854bd674ef82720fe99df1c922405f432c
gurpreets@neuronimbus.com-at-116341680296 <grupreets@neuronimbus.com>
2022-10-10 11:46:21 +0530

    css changes

--- src/pages/AddPermissions.jsx
+++ src/pages/AddPermissions.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Container } from 'react-bootstrap';
 import { controllers, actions } from '../assets/data/permissions';
+import LeftNavbar from "../components/LeftNavbar";
+import TopHeader from "../components/TopHeader";
+import Table from 'react-bootstrap/Table';
 
 const AddPermissions = (props) => {
     const [controllerList, setControllerList] = useState(null);
@@ -59,24 +62,43 @@ const AddPermissions = (props) => {
     console.log('SAVED PERMISSION:', savedPermissions);
 
     return (
-        <div className="container">
+        <div id="main">
+          <section className="mainsection">
+            <Container>
+              <div className="admin-wrapper">
+                <aside className="app-sidebar">
+                  <LeftNavbar />
+                </aside>
+                <div className="sec-column">
+                  <TopHeader />
+                  <div className="entry-container">
+                    <header className="title-head">
+                      <h1 className="title-lg">Add Permissions</h1>
+                    </header>
+                    <div className="permissions-sec">
+                    <Table bordered>
+                      <tbody>
+                      
             {
                 controllerList && controllerList.map(controller => (
-                    <div>
-                        <h1 onClick={() => handleSelectAll(controller.id)}>
-                            {controller.label}
-                        </h1>
+                    <tr>
+                    <td>
+                        <p className="head" onClick={() => handleSelectAll(controller.id)}>
+                            <strong>{controller.label}</strong>
+                        </p>
+                    </td>
+                    <td>
+                        <div className="d-flex">
                         {
                             actionList && actionList.map(action => (
                                 <div className="action">
-                                    <p>{action.label}</p>
-                                    <Form.Group className="mb-3">
+                                    <Form.Group className="mb-3 me-4">
                                         <div className="btn-checkbox">
                                             <Form.Check
                                                 type="checkbox"
                                                 id={`${controller.value}_${action.value}`}
                                                 checked={savedPermissions[`${controller.id}`].includes(action.id)}
-                                                label=""
+                                                label={action.label}
                                                 onChange={() => {
                                                     handleSavePermission(controller.id, action.id);
                                                 }} />
@@ -85,10 +107,22 @@ const AddPermissions = (props) => {
                                 </div>  
                             ))
                         }
-                    </div>
+                        </div>
+                        </td>
+                    </tr>
                 ))
             }
+            </tbody>
+                    </Table>
         </div>
+                  </div>
+                </div>
+              </div>
+            </Container>
+          </section>
+        </div>
+        
+        
     );
 }
 
