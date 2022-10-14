8496cd0b4839606e5323543442bde33b55d1881c
pardeepverma007 <pardeepv@neuronimbus.com>
2022-10-14 11:37:23 +0530

    Some Changes in File Repo

--- src/pages/FilerepoUploadFile.jsx
+++ src/pages/FilerepoUploadFile.jsx
@@ -39,6 +39,7 @@ const FilerepoUploadFile = () => {
         shared_role: '',
         accessible_to_role: 1
     });
+    console.log(userCount, "selectedUser")
     const [formSettings, setFormSettings] = useState({
         assigned_franchisee: [],
     });
@@ -130,7 +131,8 @@ const FilerepoUploadFile = () => {
     //======================== GET Children List==================
 
     const getChildren = async () => {
-        let selectedUserr = selectedUser.length == 0 ? [] : selectedUser.map(item => item.id)
+        let selectedUserr = selectedUser.length === 0 ? [] : selectedUser.map(item => item.id)
+        
         let response = await axios.get(`${BASE_URL}/enrollment/listOfChildren?childId=${JSON.stringify(selectedUserr)}`, {
             headers: {
                 authorization: `Bearer ${localStorage.getItem('token')}`,
