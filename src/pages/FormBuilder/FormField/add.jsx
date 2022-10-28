faee3b0b997412cc2b72f1a076742c27d35b12cf
Rudra Gaur <irudragaur@gmail.com>
2022-10-28 09:31:03 +0530

    changes

--- src/pages/FormBuilder/FormField/add.jsx
+++ src/pages/FormBuilder/FormField/add.jsx
@@ -766,9 +766,9 @@ const AddFormField = (props) => {
                                   <Form.Control
                                     type="text"
                                     name="field_label"
-                                    // disabled={
-                                    //   updateFlag && newFieldAddIndex !== index
-                                    // }
+                                    disabled={
+                                      updateFlag && newFieldAddIndex !== index
+                                    }
                                     id={'field_label' + index}
                                     maxLength={255}
                                     value={form[index]?.field_label}
@@ -801,9 +801,9 @@ const AddFormField = (props) => {
                                         index
                                       );
                                     }}
-                                    // disabled={
-                                    //   updateFlag && newFieldAddIndex !== index
-                                    // }
+                                    disabled={
+                                      updateFlag && newFieldAddIndex !== index
+                                    }
                                   >
                                     <option
                                       value="text_headings"
@@ -936,12 +936,12 @@ const AddFormField = (props) => {
                                             <Form.Control
                                               type="text"
                                               name="option"
-                                              // disabled={
-                                              //   updateFlag &&
-                                              //   newFieldAddIndex !== index &&
-                                              //   inner_index !==
-                                              //   newOptionAddIndex
-                                              // }
+                                              disabled={
+                                                updateFlag &&
+                                                newFieldAddIndex !== index &&
+                                                inner_index !==
+                                                newOptionAddIndex
+                                              }
                                               id={
                                                 'option' + index + inner_index
                                               }
@@ -1131,12 +1131,12 @@ const AddFormField = (props) => {
                                           index
                                         );
                                       }}
-                                      // disabled={
-                                      //   form[index]?.field_type ===
-                                      //   'headings' ||
-                                      //   form[index]?.field_type ===
-                                      //   'text_headings'
-                                      // }
+                                      disabled={
+                                        form[index]?.field_type ===
+                                        'headings' ||
+                                        form[index]?.field_type ===
+                                        'text_headings'
+                                      }
                                     />
                                   </div>
                                 </div>
@@ -1404,11 +1404,11 @@ const AddFormField = (props) => {
                                                 <Form.Control
                                                   type="text"
                                                   name="option"
-                                                  // disabled={
-                                                  //   updateFlag &&
-                                                  //   newConditionOptionAddIndex !==
-                                                  //   inner_index
-                                                  // }
+                                                  disabled={
+                                                    updateFlag &&
+                                                    newConditionOptionAddIndex !==
+                                                    inner_index
+                                                  }
                                                   value={
                                                     inner_item[
                                                     Object.keys(inner_item)[0]
@@ -2218,7 +2218,7 @@ const AddFormField = (props) => {
                                     backgroundColor: '#455C58',
                                     width: '80px',
                                   }}
-                                  // disabled={sectionTitle === '' ? true : false}
+                                  disabled={sectionTitle === '' ? true : false}
                                   onClick={() => {
                                     counter++;
                                     setCount(counter);
